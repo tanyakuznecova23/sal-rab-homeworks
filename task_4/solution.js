076a05f1b1e08631e6045db69b0b1880864afb24
Tatiana Kuznetsova <t_kuznecova@icloud.com>
2023-01-19 01:02:47 +0300

    committ7

--- task_4/solution.js
+++ task_4/solution.js
@@ -3,16 +3,18 @@
 function parseProducts(json) {
     // Аргументом функции является JSON
 
-    // Преобразуйте строку json, переданную как аргумент функции,
-    // в объект с помощью функции JSON.parse(json)
-    // и запишите в переменную data
-    // Верните как результат функции свойство products объекта data
+     // Преобразуйте строку json, переданную как аргумент функции,
+    let data = JSON.parse(json); // в объект с помощью функции JSON.parse(json)
+    let products = data.products; // и запишите в переменную data
+    return products; // Верните как результат функции свойство products объекта data
 }
 
-// Напишите функцию renderProductsCards(json)
-// Аргументом функции является JSON
-// Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
-// Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
-// Запишите в переменную length значение свойства products.length
-// Напишите цикл, в котором перебираете все products от 0 до (length - 1)
-// и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+function renderProductsCards(json) { // Напишите функцию renderProductsCards(json)
+    clearProducts(); // Аргументом функции является JSON
+    renderProductsCards(let products); // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
+    let products = parseProducts(json); // Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
+    let length = products.length; // Запишите в переменную length значение свойства products.length
+    for (let i = 0; i < length; i +=1) { // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
+    addProduct(products[i]); // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+    }
+}   
